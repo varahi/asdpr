0d165667284d31c5ed179d105beadf2b80ef3d4a
Dmitry Vasilev <dmitry@t3dev.ru>
2020-07-28 13:17:46 +0300

    Feature #442 - add flexform to plugin

--- src/andprmembers/ext_tables.php
+++ src/andprmembers/ext_tables.php
@@ -13,6 +13,14 @@ call_user_func(
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('andprmembers', 'Configuration/TypoScript', 'andprmembers');
 
+        // Add FlexForm
+        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['andprmembers_andprmembers'] = 'recursive,select_key,pages';
+        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['andprmembers_andprmembers'] = 'pi_flexform';
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+            'andprmembers_andprmembers',
+            'FILE:EXT:andprmembers/Configuration/FlexForms/flexform_andprmembers.xml'
+        );
+
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_andprmembers_domain_model_location', 'EXT:andprmembers/Resources/Private/Language/locallang_csh_tx_andprmembers_domain_model_location.xlf');
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_andprmembers_domain_model_location');
 
